73717729ff2eb4b3bc2479b51989dd919c51ae24
Skúli Arnarsson <skuliarnarsson@gmail.com>
2023-03-28 11:24:41 +0000

    remove jwt secret

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -20,7 +20,7 @@ export const authOptions: NextAuthOptions = {
       return token
     },
   },
-  secret: process.env.JWT_SECRET,
+  // secret: process.env.JWT_SECRET,
 }
 
 export default NextAuth(authOptions)
